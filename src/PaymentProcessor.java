2b2d567b4a53f0c0f15619672a7e0f66ee41c34c
AbderrazzakAiba <aibaabderrazzak7@gmail.com>
2026-05-01 13:42:49 +0100

    Refactor: implement strategy pattern for payment processing

--- src/PaymentProcessor.java
+++ src/PaymentProcessor.java
@@ -1,13 +1,41 @@
+// Strategy Interface
+interface PaymentStrategy {
+    void pay(double amount);
+}
+
+// Concrete Strategy 1
+class CreditCardPayment implements PaymentStrategy {
+    @Override
+    public void pay(double amount) {
+        System.out.println("Processing credit card payment of $" + amount);
+    }
+}
+
+// Concrete Strategy 2
+class PayPalPayment implements PaymentStrategy {
+    @Override
+    public void pay(double amount) {
+        System.out.println("Processing PayPal payment of $" + amount);
+    }
+}
+
+// Concrete Strategy 3
+class BitcoinPayment implements PaymentStrategy {
+    @Override
+    public void pay(double amount) {
+        System.out.println("Processing Bitcoin payment of $" + amount);
+    }
+}
+
+// Context Class
 public class PaymentProcessor {
-    public void processPayment(String paymentMethod, double amount) {
-        if (paymentMethod.equals("CreditCard")) {
-            System.out.println("Processing credit card payment of $" + amount);
-        } else if (paymentMethod.equals("PayPal")) {
-            System.out.println("Processing PayPal payment of $" + amount);
-        } else if (paymentMethod.equals("Bitcoin")) {
-            System.out.println("Processing Bitcoin payment of $" + amount);
-        } else {
-            throw new IllegalArgumentException("Unknown payment method");
-        }
+    private PaymentStrategy paymentStrategy;
+
+    public void setPaymentStrategy(PaymentStrategy paymentStrategy) {
+        this.paymentStrategy = paymentStrategy;
+    }
+
+    public void processPayment(double amount) {
+        paymentStrategy.pay(amount);
     }
 }
